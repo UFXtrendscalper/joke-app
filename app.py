171bbeb03893225fba90e9ea2c8f259b4647dd92
Sean Gilbank <sean7725@hotmail.com>
2025-07-30 15:01:45 -0700

    Initial commit - joke app

---
+++ app.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+import random
+
+jokes = [
+    "Why don't scientists trust atoms? Because they make up everything!",
+    "I'm reading a book about anti-gravity. It's impossible to put down!",
+    "What do you call fake spaghetti? An impasta!",
+    "Why did the scarecrow win an award? He was outstanding in his field!"
+]
+
+def tell_joke():
+    joke = random.choice(jokes)
+    label.config(text=joke)
+
+root = tk.Tk()
+root.title("Random Joke Generator")
+root.geometry("400x200")
+
+label = tk.Label(root, text="Click the button for a joke!", wraplength=380, justify="center", font=("Arial", 12))
+label.pack(pady=20)
+
+button = tk.Button(root, text="Tell me a joke", command=tell_joke)
+button.pack()
+
+root.mainloop()
